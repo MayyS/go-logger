2180539aff486b5130dc7bcf5914d718e2910d17
ssw <864876906@qq.com>
2024-07-13 19:40:20 +0800

    ✨ feat(all): 初始化日志

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/Mayys/log
+
+go 1.22.5
+
+require go.uber.org/zap v1.27.0
+
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	k8s.io/klog v1.0.0
+)
---
+++ internal/encoder.go
@@ -0,0 +1,15 @@
+package internal
+
+import (
+	"time"
+
+	"go.uber.org/zap/zapcore"
+)
+
+func TimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
+}
+
+func MilliSecondsDurationEncoder(d time.Duration, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendFloat64(float64(d) / float64(time.Millisecond))
+}
